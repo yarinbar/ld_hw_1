5112e28c86bcaf2d31f3192e89deeacc96f63c07
yarinbar <yarinbar@campus.technion.ac.il>
2018-11-05 20:08:24 +0200

    first day is over

--- simon1.v
+++ simon1.v
@@ -5,31 +5,39 @@ module simon1 (clock, reset, in, out);
 // B 10
 // Y 11
 
-	localparam [4]
-		R = 0,
-		G = 1,
-		B = 2,
-		Y = 3;
+
 
    	input wire [1:0] in;
 	input wire clock;
 	input wire reset;
 	output reg [1:0] out;
 
+	reg [1:0] state, next_state;
+
 		
 
 
 	always @ (posedge clock, posedge reset) 
 	begin
-		if(reset)
-
+		if(reset) begin
+			// change 0 to initial state
+			state <= 0;
+			next_state <= 2'dX;		
+		end
+		
+		else begin
+			state <= next_state;
+		end
 	end
 
-	always @ (in) begin
-	temp = in; //attacker's in
 
-		assign out = 2'dX;
+
+	always @ (in) begin
 		
+		state <= next_state;
+		next_state <= in; //attacker's in
+		
+		assign out = 2'dX;
 				
 			
 		// if the input is invalid
@@ -37,7 +45,7 @@ module simon1 (clock, reset, in, out);
 			assign out = 2'dX;
 
 		else
-			if(in === temp) //player's in
+			if(in === state) //player's in
 				assign out = 2'b10;
 			else
 				assign out = 2'b01;
